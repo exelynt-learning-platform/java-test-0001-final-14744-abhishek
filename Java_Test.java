fbc8456652b8f9c14639baeb91362790a2a0703c
Abhishek Rajendra Mohite <150648384+AbhishekMohite21@users.noreply.github.com>
2026-03-14 16:58:34 +0530

    Add files via upload

---
+++ Java_Test.java
@@ -0,0 +1,51 @@
+public class PatternDiamond {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            if (i == 1) {
+                System.out.println("*");
+            } else {
+                System.out.print("*");
+
+                // Inner spaces
+                for (int j = 1; j <= (2 * i - 3); j++) {
+                    System.out.print(" ");
+                }
+
+                System.out.println("*");
+            }
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            if (i == 1) {
+                System.out.println("*");
+            } else {
+                System.out.print("*");
+
+                for (int j = 1; j <= (2 * i - 3); j++) {
+                    System.out.print(" ");
+                }
+
+                System.out.println("*");
+            }
+        }
+    }
+}
